c7f8f14d01b0edfa0825911ef33dbdac4d4f58d6
Lucas Gabriel Adelino Araújo <lucasgabrielz2208@gmail.com>
2025-08-17 09:11:18 -0300

    Correção

--- knexfile.js
+++ knexfile.js
@@ -11,23 +11,23 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: '127.0.0.1',
+      host: 'postgres-db',
       port: 5432,
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
     },
     migrations: {
-        directory: './db/migrations',
-      },
+      directory: './db/migrations',
+    },
     seeds: {
-        directory: './db/seeds',
-      },
+      directory: './db/seeds',
+    },
   },
   ci: {
     client: 'pg',
     connection: {
-      host: 'postgres', 
+      host: 'postgres',
       port: 5432,
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
